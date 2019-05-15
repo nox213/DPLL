623c13026edc8624d6451a8f522dce476de24708
yoonjae <yoonjae@kaist.ac.kr>
2019-05-15 15:14:04 +0900

    Implement ds

--- solvepy3.py
+++ solvepy3.py
@@ -1,16 +1,32 @@
 import argparse
 
 class Variable:
+        def __init__(self, value):
+                self.value = value 
 
-        def __init__(self, boolean):
-                self
-                
+        def assign(self, value):
+                self.value = value
+
+class Literal:
+        def __init__(self, variable, sign):
+                self.variable = variable
+                self.sign = sign
+
+class Clause
+        def __init__(self, literals):
+                self.literals = literals
+        def size(self):
+                return len(self.literals)
+
 
 def main():
         parser = argparse.ArgumentParser()
         parser.add_argument("file", help="input for dpll")
         args = parser.parse_args()
         print(args.file)
+
+        variables = []
+        clauses = set()
         
         f = open(args.file, 'r')
         lines = f.readlines()
@@ -25,6 +41,16 @@ def main():
                                 is_cnf = False
                         num_var = int(param[2])
                         num_clause = int(param[3])
+                        for i in range(0, num_var):
+                                variables.append(Variable(False))
+                else:
+                        numbers = line.split()
+                        for number in numbers:
+                                if int(number) == 0:
+                                        break
+
+                        
+                        
 
         print(num_var)
         print(num_clause)
