97768bddcde8c22c9cca48a1d52c42d17e4cb15e
yoonjae <yoonjae@kaist.ac.kr>
2019-05-17 22:35:23 +0900

    Modify backtracking

--- solvepy3.py
+++ solvepy3.py
@@ -5,88 +5,108 @@ def find_unit_clause(clauses, variables):
         f_a = deepcopy(clauses)
         unit_clause = -1
         empty_clause = -1
-        i = 0
-        for clause in f_a:
+        for i, clause in list(enumerate(clauses)):
                 for literal in clause:
                         value = variables[abs(literal)]
+                        if value == -1:
+                                continue
                         if literal > 0:
                                 sign = 1
                         else:
                                 sign = -1
-                        if value == -1:
-                                continue
-                        if value * sign == 1:
-                                f_a.remove(clause)
+                        idx = i - (len(clauses) - len(f_a))
+                        if (value == 1 and sign == 1) or (value == 0 and sign == -1):
+                                f_a.pop(idx)
+                                break
                         else:
-                                clause.remove(literal)
-                                if len(clause) == 0:
+                                f_a[idx] -= set([literal])
+                                if len(f_a[idx]) == 0:
                                         empty_clause = i
-                                elif len(clause) == 1
-                                        unit_clause = i
-                i += 1
-                                
+
+        for i, clause in list(enumerate(f_a)):
+                if len(clause) == 1:
+                        unit_clause = i
+                        break
+
         return f_a, unit_clause, empty_clause
 
-def resolve(c1, c2, p)
+def resolve(c1, c2, p):
         if p in c1:
-                resolvent = (c1 - set(p)) | (c2 - set([-p]))
+                resolvent = (c1 - set([p])) | (c2 - set([-p]))
         else:
-                resolvent = (c1 - set(-p)) | (c2 - set([p]))
+                resolvent = (c1 - set([-p])) | (c2 - set([p]))
 
         return resolvent
 
-def clause_learning(clauses, decision, empty_clause):
+def clause_learning(clauses, decision, empty_clause, variables):
         conflict = clauses[empty_clause]
         for d in reversed(decision):
-                if d[2] == True || d[0] not in conflict:
+                if (d[2] == True) or (d[0] not in conflict):
                         continue
                 else:
-                        conflict = resolvent(conflict, d[3], d[0])
+                        conflict = resolve(conflict, clauses[d[3]], d[0])
+        variables[decision.pop()[0]] = -1
+        while True:
+                is_unit = True
+                for d in decision:
+                        if d[0] in conflict or -d[0] in conflict:
+                                is_unit = False
+                                break
+                if is_unit == True:
+                        break
+                variables[decision.pop()[0]] = -1
+                        
         return conflict
 
-def dpll(clauses, variables):
-        clauses_l = list(clauses)
-        is_unsat = False
-        decision = []
-
+def decise_variable(clauses, variables, decision):
+        i = 0
+        for variable in variables:
+                if variable == -1:
+                        return i
+                i += 1
+                
+def dpll(clauses, variables, decision):
         #unit propagation
-        while True:
-                while True:
-                        f_a, unit_clause, empty_clause = find_unit_clause(clauses, variables)
-                        if len(f_a) == 0
-                                return decision
-                        elif empty_clause >= 0:
-                                break
-                        elif unit_clause >= 0:
-                                c = clauses_l[unit_clause]
-                                l = list(c)[0]
-                                #(literal, value, decision, C_i)
-                                if l > 0:
-                                        decision.append((l, 1, False, unit_clause)        
-                                        variables[abs(l)] = 1
-                                else:
-                                        decision.append((l, 0, False, unit_clause)        
-                                        variables[abs(l)] = 0
-                        else:
-                                break
-
-                if empty_clause >= 0:
-                        learned_clause = clause_learning(clauses_l, decision, empty_clause)
-                        if len(learned_clause) == 0:
-                                is_unsat = True
-                                break
-                        clauses.add(learned_clause)
+        f_a, unit_clause, empty_clause = find_unit_clause(clauses, variables)
+        if len(f_a) == 0:
+                return decision
+        elif empty_clause >= 0:
+                break
+        elif unit_clause >= 0:
+                c = f_a[unit_clause]
+                l = list(c)[0]
+                #(variable, value, decision, C_i)
+                if l > 0:
+                        variables[abs(l)] = 1
+                        p_a = dpll(clauses, variables, decision + [((var, 1, False, unit_clause))])
+                        variables[abs(l)] = 0
                 else:
-                        decision.append
+                        variables[abs(l)] = 0
+                        p_a = dpll(clauses, variables, decision + [((var, 0, False, unit_clause))])
+                        variables[abs(l)] = 1
 
-        if is_unsat == True
-                print('s UNSATISFIABLE')
+                if len(p_a) > 0:
+                        return p_a
+                else:
+                        return list()
         else:
-                print('s SATISFIABLE')
-                for i in range(len(decision)):
+                break
 
+        if empty_clause >= 0:
+                learned_clause = clause_learning(clauses, decision, empty_clause, variables)
+                clauses.append(learned_clause)
+                if len(learned_clause) == 0:
+                        return list()
 
-                                
+        var = decise_variable(clauses, variables, decision)
+        variables[var] = 1
+        p_a = dpll(clauses, variables, decision + [((var, 1, True))])
+        if len(p_a) > 0:
+                return p_a
+        variables[var] = 0
+        p_a = dpll(clauses, variables, decision + [((var, 0, True))])
+        variables[var] = -1
+        return p_a
 
 def main():
         parser = argparse.ArgumentParser()
@@ -121,14 +141,12 @@ def main():
                                         break
                                 literals.append(n)
                                 temp = frozenset(literals)
-                                clauses.add(temp)
-
-#      dpll(clauses, variables)
-#    print(num_var)
-#        print(num_clause)
-#        print(is_cnf)
-         
+                        clauses.add(temp)
+        p_a = dpll(list(clauses), variables, list())
+        if len(p_a) > 0:
+                print('s SATISFIABLE')
+        else:
+                print('s SATISFIABLE')
 
 if __name__ == '__main__':
         main()
-
