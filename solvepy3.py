9a9e81cf539d67d0765e88f526ced19f4bdc9115
yoonjae <yoonjae@kaist.ac.kr>
2019-05-18 22:50:57 +0900

    Fix decision strategy

--- solvepy3.py
+++ solvepy3.py
@@ -2,48 +2,62 @@ import argparse
 import random
 from copy import deepcopy
 
-def find_unit_clause(clauses, variables):
+def find_unit_clause(clauses, variables, vars_freq):
         f_a = deepcopy(clauses)
-        unit_list = []
-        empty_list = []
-        true_list = []
+        unit_clauses = []
+        empty_clauses = []
+        true_clauses = []
+
+        for var in vars_freq:
+                var[1] = 0
+                var[2] = 0
+                var[3] = 0
+
         for i, clause in list(enumerate(clauses)):
                 for literal in clause:
+                        vars_freq[abs(literal)][1] += 1
                         value = variables[abs(literal)]
                         if value == -1:
                                 if len(f_a[i]) == 1:
-                                        unit_list.append(i)
+                                        unit_clauses.append(i)
                                 continue
                         if literal > 0:
                                 sign = 1
+                                vars_freq[abs(literal)][2] += 1
                         else:
                                 sign = -1
+                                vars_freq[abs(literal)][3] += 1
                         if (value == 1 and sign == 1) or (value == 0 and sign == -1):
-                                true_list.append(i)
+                                true_clauses.append(i)
                                 break
                         else:
                                 f_a[i] -= set([literal])
+                                vars_freq[abs(literal)][1] -= 1
+                                if sign == 1:
+                                        vars_freq[abs(literal)][2] -= 1
+                                else:
+                                        vars_freq[abs(literal)][3] -= 1
                                 if len(f_a[i]) == 0:
-                                        empty_list.append(i)
+                                        empty_clauses.append(i)
                                 if len(f_a[i]) == 1:
-                                        unit_list.append(i)
+                                        unit_clauses.append(i)
         
-        for empty in empty_list:
-                if empty in unit_list:
-                        unit_list.remove(empty)
-        for true in true_list:
-                if true in unit_list:
-                        unit_list.remove(true)
+        for empty in empty_clauses:
+                if empty in unit_clauses:
+                        unit_clauses.remove(empty)
+        for true in true_clauses:
+                if true in unit_clauses:
+                        unit_clauses.remove(true)
         unit_clause = -1
         empty_clause = -1
-        if len(empty_list) > 0:
-                empty_clause = empty_list.pop()
-        if len(unit_list) > 0:
-                unit_clause = unit_list.pop()
-        if len(true_list) == len(clauses):
+        if len(empty_clauses) > 0:
+                empty_clause = empty_clauses.pop()
+        if len(unit_clauses) > 0:
+                unit_clause = unit_clauses.pop()
+        if len(true_clauses) == len(clauses):
                 f_a = []
 
-        return f_a, unit_clause, empty_clause
+        return f_a, unit_clause, empty_clause, true_clauses
 
 def resolve(c1, c2, p):
         if p in c1:
@@ -81,19 +95,38 @@ def clause_learning(clauses, decision, empty_clause, variables):
         return conflict
 
 def decise_variable(clauses, variables, decision, variables_freq):
+        variables_freq.sort(reverse=True, key=lambda frequency: frequency[1])
         for variable in variables_freq:
                 var = variable[0]
                 if var != 0 and variables[var] == -1:
                         return var
+
+def min_conflict(var, f_a, true_clauses):
+
+        true = 0
+        false = 0
+        for i, clause in list(enumerate(f_a)):
+                if i in true_clauses:
+                        continue
+                if var in clause:
+                        true += 1
+                else:
+                        false += 1
+
+        if true > false:
+                return 1
+        else:
+                return 0
+
                 
-def dpll(clauses, variables,  variables_freq):
+def dpll(clauses, variables, vars_freq):
         decision = []
         learned = []
         #unit propagation
         while True:
                 while True:
 #print(decision)
-                        f_a, unit_clause, empty_clause = find_unit_clause(clauses, variables)
+                        f_a, unit_clause, empty_clause, true_clauses = find_unit_clause(clauses, variables, vars_freq)
                         if len(f_a) == 0:
                                 return decision
                         elif empty_clause >= 0:
@@ -121,10 +154,11 @@ def dpll(clauses, variables,  variables_freq):
                         if len(learned_clause) == 0:
                                 return list()
                 else:
-                        var = decise_variable(clauses, variables, decision, variables_freq)
+                        var = decise_variable(clauses, variables, decision, vars_freq)
                         variables[var] = 1
-#          decision.append((var, 1, True))
-                        decision.append((var, random.randrange(0,2), True))
+                        value = min_conflict(var, f_a, true_clauses)
+                        decision.append((var, value, True))
+#decision.append((var, random.randrange(0,2), True))
 
 def main():
         parser = argparse.ArgumentParser()
@@ -133,7 +167,7 @@ def main():
         print(args.file)
 
         variables = []
-        variables_freq = []
+        vars_freq = []
         clauses = set()
         
         f = open(args.file, 'r')
@@ -151,7 +185,7 @@ def main():
                         num_clause = int(param[3])
                         for i in range(0, num_var + 1):
                                 variables.append(-1)
-                                variables_freq.append((i, 0))
+                                vars_freq.append([i, 0, 0, 0])
                 else:
                         numbers = line.split()
                         literals = []
@@ -160,14 +194,12 @@ def main():
                                 if n == 0:
                                         break
                                 literals.append(n)
-                                variables_freq[abs(n)] = (variables_freq[abs(n)][0], variables_freq[abs(n)][1] + 1)
                                 temp = frozenset(literals)
                         clauses.add(temp)
 
         #variable 0 is not used
         variables[0] = 100
-        variables_freq.sort(reverse=True, key=lambda frequency: frequency[1])
-        p_a = dpll(list(clauses), variables,  variables_freq)
+        p_a = dpll(list(clauses), variables, vars_freq)
         if len(p_a) > 0:
                 print('s SATISFIABLE')
         else:
